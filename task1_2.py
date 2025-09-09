29d6f64ddc3f80712d3fea9b076f0398d7541e7d
linakurmangazieva <lina.kurmangazieva@gmail.com>
2025-09-09 09:59:10 +0300

    Add files via upload

---
+++ task1_2.py
@@ -0,0 +1,10 @@
+#сохраним список в переменной
+li = ['a', 'b', 'c', 'd', 'e']
+#создадим стек
+from collections import deque
+stack = deque()
+stack.append('a')
+stack.append('b')
+stack.append('c')
+stack.append('d')
+stack.append('e')
